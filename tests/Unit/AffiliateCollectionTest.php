5bd8ed47dbcf76109c2332eed7be8ee5b22fb6b2
Nikola <nikolamitrovic1406@gmail.com>
2025-06-15 20:54:56 +0200

    Collections unit tests

---
+++ tests/Unit/AffiliateCollectionTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Collections\AffiliateCollection;
+use App\DTOs\AffiliateDTO;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class AffiliateCollectionTest extends TestCase
+{
+    public function test_can_construct_with_affiliate_dtos(): void
+    {
+        $dto1 = new AffiliateDTO(1, 'First', 53.0, -6.0);
+        $dto2 = new AffiliateDTO(2, 'Second', 53.1, -6.1);
+
+        $collection = new AffiliateCollection([$dto1, $dto2]);
+
+        $this->assertCount(2, $collection);
+        $this->assertContains($dto1, iterator_to_array($collection));
+        $this->assertContains($dto2, iterator_to_array($collection));
+    }
+
+    public function test_rejects_non_affiliate_items(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new AffiliateCollection([(object) ['random' => 'object']]);
+    }
+
+    public function test_toArray_returns_associative_arrays(): void
+    {
+        $dto = new AffiliateDTO(1, 'Affiliate DTO', 52.5, -6.5);
+        $collection = new AffiliateCollection([$dto]);
+
+        $expected = [
+            [
+                'affiliateId' => 1,
+                'name'         => 'Affiliate DTO',
+                'latitude'     => 52.5,
+                'longitude'    => -6.5,
+            ],
+        ];
+
+        $this->assertEquals($expected, $collection->toArray());
+    }
+
+    public function test_iterable_behavior(): void
+    {
+        $dtos = [
+            new AffiliateDTO(3, 'Third', 53.3, -6.3),
+            new AffiliateDTO(4, 'Fourth', 53.4, -6.4),
+        ];
+        $collection = new AffiliateCollection($dtos);
+
+        $iterated = [];
+        foreach ($collection as $dto) {
+            $this->assertInstanceOf(AffiliateDTO::class, $dto);
+            $iterated[] = $dto;
+        }
+
+        $this->assertCount(2, $iterated);
+        $this->assertSame($dtos, $iterated);
+    }
+}
---
+++ tests/Unit/TypedCollectionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Collections\TypedCollection;
+use App\DTOs\AffiliateDTO;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class TypedCollectionTest extends TestCase
+{
+    public function test_accepts_correct_type_and_allows_iteration(): void
+    {
+        $dto1 = new AffiliateDTO(1, 'One', 0, 0);
+        $dto2 = new AffiliateDTO(2, 'Two', 1, 1);
+
+        $collection = new TypedCollection(AffiliateDTO::class, [$dto1, $dto2]);
+
+        $this->assertCount(2, $collection);
+
+        $iterated = [];
+        foreach ($collection as $item) {
+            $this->assertInstanceOf(AffiliateDTO::class, $item);
+            $iterated[] = $item;
+        }
+        $this->assertSame([$dto1, $dto2], $iterated);
+    }
+
+    public function test_toArray_converts_dtos_to_arrays(): void
+    {
+        $dto = new AffiliateDTO(1, 'Affiliate DTO', 1, 1);
+
+        $collection = new TypedCollection(AffiliateDTO::class, [$dto]);
+
+        $array = $collection->toArray();
+
+        $this->assertIsArray($array);
+        $this->assertCount(1, $array);
+        $this->assertEquals([
+            [
+                'affiliateId' => 1,
+                'name'         => 'Affiliate DTO',
+                'latitude'     => 1.0,
+                'longitude'    => 1.0,
+            ]
+        ], $array);
+    }
+
+    public function test_rejects_invalid_item_type(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new TypedCollection(AffiliateDTO::class, [(object) ['foo' => 'bar']]);
+    }
+
+    public function test_empty_initial_collection_is_valid(): void
+    {
+        $collection = new TypedCollection(AffiliateDTO::class, []);
+        $this->assertCount(0, $collection);
+        $this->assertSame([], $collection->toArray());
+    }
+}
